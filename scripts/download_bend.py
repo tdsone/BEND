44b3ac18467de41f5562901266fb3a6914f02cf5
Timon Schneider <mail@timonschneider.de>
2024-07-04 17:27:46 +0100

    Formatting and custom download destination

--- scripts/download_bend.py
+++ scripts/download_bend.py
@@ -3,38 +3,47 @@ from bs4 import BeautifulSoup
 import os
 
 
-base_link = 'https://sid.erda.dk/'
-link = 'https://sid.erda.dk/cgi-sid/ls.py?share_id=f6hdp1zTzh&current_dir=.&flags=f'
+base_link = "https://sid.erda.dk/"
+link = "https://sid.erda.dk/cgi-sid/ls.py?share_id=f6hdp1zTzh&current_dir=.&flags=f"
+
 
 def get_soup(link):
     source_code = requests.get(link)
-    soup = BeautifulSoup(source_code.content,"lxml")
+    soup = BeautifulSoup(source_code.content, "lxml")
     f = []
-    f.extend(soup.find_all('a', {'class' : ['leftpad directoryicon', ]}))
-    f.extend(soup.find_all('a', {'title' : 'open'}))
+    f.extend(
+        soup.find_all(
+            "a",
+            {
+                "class": [
+                    "leftpad directoryicon",
+                ]
+            },
+        )
+    )
+    f.extend(soup.find_all("a", {"title": "open"}))
     return f
 
 
-# download file in link 
+# download file in link
 def download_file(link, destination):
     r = requests.get(link, allow_redirects=True)
-    open(destination, 'wb').write(r.content)
+    open(destination, "wb").write(r.content)
 
 
-
-
-def rec(link, destination = './'):
+def rec(link, destination="./"):
     f = get_soup(link)
     for child in f:
-        if child.get('title') == 'open':
+        if child.get("title") == "open":
             link = f'{base_link}{child.get("href")}'
             child_path = child.get("href")[27:]
-            os.makedirs(f'{destination}/{os.path.dirname(child_path)}', exist_ok=True)
-            print(f'{destination}/{child_path}')
-            download_file(link, f'{destination}/{child_path}')
+            os.makedirs(f"{destination}/{os.path.dirname(child_path)}", exist_ok=True)
+            print(f"{destination}/{child_path}")
+            download_file(link, f"{destination}/{child_path}")
         else:
             link = f'{base_link}cgi-sid/{child.get("href")}'
             rec(link, destination)
 
-print('DOWNLOADING')
-rec(link, destination = '.')
+
+print("DOWNLOADING")
+rec(link, destination="/mnt/data/shared/dna-tokenizer/BEND")
