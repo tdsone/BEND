d8f9e83cafac22b7d232e70f686b85a3a758cb9d
Timon Schneider <mail@timonschneider.de>
2024-08-05 13:28:00 +0100

    Code formatting

--- scripts/precompute_embeddings.py
+++ scripts/precompute_embeddings.py
@@ -1,4 +1,4 @@
-import hydra 
+import hydra
 from omegaconf import DictConfig, OmegaConf
 import torch
 import os
@@ -6,7 +6,9 @@ import bend.io.sequtils as sequtils
 import pandas as pd
 import numpy as np
 import sys
-# load config 
+
+
+# load config
 @hydra.main(config_path="../conf/embedding/", config_name="embed", version_base=None)
 def run_experiment(cfg: DictConfig) -> None:
     """
@@ -17,45 +19,54 @@ def run_experiment(cfg: DictConfig) -> None:
     cfg : DictConfig
         Hydra configuration object.
     """
-    print('Embedding data for', cfg.task)
-    # read the bed file and get the splits :  
-    if not 'splits' in cfg or cfg.splits is None:
-        splits = sequtils.get_splits(cfg[cfg.task].bed) 
+    print("Embedding data for", cfg.task)
+    # read the bed file and get the splits :
+    if not "splits" in cfg or cfg.splits is None:
+        splits = sequtils.get_splits(cfg[cfg.task].bed)
     else:
         splits = cfg.splits
-    print('Embedding with', cfg.model) 
+    print("Embedding with", cfg.model)
     # instatiante model
     embedder = hydra.utils.instantiate(cfg[cfg.model])
     for split in splits:
-        print(f'Embedding split: {split}')
-        output_dir = f'{cfg.data_dir}/{cfg.task}/{cfg.model}/'
-        
+        print(f"Embedding split: {split}")
+        output_dir = f"{cfg.data_dir}/{cfg.task}/{cfg.model}/"
+
         os.makedirs(output_dir, exist_ok=True)
 
-        # embed in chunks 
-        # get length of bed file and divide by chunk size, if a spcific chunk is not set 
-        df = pd.read_csv(cfg[cfg.task].bed, sep = '\t', low_memory=False)
+        # embed in chunks
+        # get length of bed file and divide by chunk size, if a spcific chunk is not set
+        df = pd.read_csv(cfg[cfg.task].bed, sep="\t", low_memory=False)
         df = df[df.iloc[:, -1] == split] if split is not None else df
-        possible_chunks = list(range(int(len(df) /cfg.chunk_size)+1))
-        if cfg.chunk is None: 
+        possible_chunks = list(range(int(len(df) / cfg.chunk_size) + 1))
+        if cfg.chunk is None:
             cfg.chunk = possible_chunks
         cfg.chunk = [cfg.chunk] if isinstance(cfg.chunk, int) else cfg.chunk
         # embed in chunks
-        for n, chunk in enumerate(cfg.chunk): 
+        for n, chunk in enumerate(cfg.chunk):
             if chunk not in possible_chunks:
-                print(f'{chunk} is not a valid chunk id. {split} chunk ids are {possible_chunks}')
+                print(
+                    f"{chunk} is not a valid chunk id. {split} chunk ids are {possible_chunks}"
+                )
                 continue
-            print(f'\t Embedding chunk {chunk} ({chunk +1}/{len(possible_chunks)})')
-            sequtils.embed_from_bed(**cfg[cfg.task], embedder = embedder, 
-                                        output_path = f'{output_dir}/{split}_{chunk}.tar.gz',
-                                        split = split, chunk = chunk, chunk_size = cfg.chunk_size,   
-                                        upsample_embeddings = cfg[cfg.model]['upsample_embeddings'] if 'upsample_embeddings' in cfg[cfg.model] else False)
-            
-            
-        
+            print(f"\t Embedding chunk {chunk} ({chunk +1}/{len(possible_chunks)})")
+            sequtils.embed_from_bed(
+                **cfg[cfg.task],
+                embedder=embedder,
+                output_path=f"{output_dir}/{split}_{chunk}.tar.gz",
+                split=split,
+                chunk=chunk,
+                chunk_size=cfg.chunk_size,
+                upsample_embeddings=(
+                    cfg[cfg.model]["upsample_embeddings"]
+                    if "upsample_embeddings" in cfg[cfg.model]
+                    else False
+                ),
+            )
 
-if __name__ == '__main__':
-    
-    print('Run Embedding')
-    
+
+if __name__ == "__main__":
+
+    print("Run Embedding")
+
     run_experiment()
--- scripts/train_on_task.py
+++ scripts/train_on_task.py
@@ -1,50 +1,58 @@
-'''
+"""
 train_on_task.py
 ----------------
 Train a model on a downstream task.
-'''
-import hydra 
+"""
+
+import hydra
 from omegaconf import DictConfig, OmegaConf, open_dict
 import torch
-from  bend.utils.task_trainer import BaseTrainer,  MSELoss, BCEWithLogitsLoss, PoissonLoss, CrossEntropyLoss
+from bend.utils.task_trainer import (
+    BaseTrainer,
+    MSELoss,
+    BCEWithLogitsLoss,
+    PoissonLoss,
+    CrossEntropyLoss,
+)
 import wandb
 from bend.models.downstream import CustomDataParallel
 import os
 import sys
+
 os.environ["WDS_VERBOSE_CACHE"] = "1"
 
-# load config 
-@hydra.main(config_path=f"../conf/supervised_tasks/", config_name=None ,version_base=None) #
+
+# load config
+@hydra.main(config_path=None, config_name=None, version_base=None)
 def run_experiment(cfg: DictConfig) -> None:
     """
     Run a supervised task experiment.
     This function is called by hydra.
-    
+
     Parameters
     ----------
     cfg : DictConfig
         Hydra configuration object.
     """
-    wandb.config = OmegaConf.to_container(
-        cfg, resolve=True, throw_on_missing=True
-        )
-    # mkdir output_dir 
-    os.makedirs(f'{cfg.output_dir}/checkpoints/', exist_ok=True)
-    print('output_dir', cfg.output_dir)
+    wandb.config = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+    # mkdir output_dir
+    os.makedirs(f"{cfg.output_dir}/checkpoints/", exist_ok=True)
+    print("output_dir", cfg.output_dir)
     # init wandb
-    run = wandb.init(**cfg.wandb, dir = cfg.output_dir, config = cfg)
-    
-    OmegaConf.save(cfg, f"{cfg.output_dir}/config.yaml") # save the config to the experiment dir
-    # set device 
+    run = wandb.init(**cfg.wandb, dir=cfg.output_dir, config=cfg)
+
+    OmegaConf.save(
+        cfg, f"{cfg.output_dir}/config.yaml"
+    )  # save the config to the experiment dir
+    # set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print('device', device)
-    # instantiate model 
+    # instantiate model
     # initialization for supervised models
-    if cfg.embedder  == 'resnet-supervised':
+    if cfg.embedder == "resnet-supervised":
         OmegaConf.set_struct(cfg, True)
         with open_dict(cfg):
             cfg.model.update(cfg.supervised_encoder[cfg.embedder])
-    if cfg.embedder == 'basset-supervised': 
+    if cfg.embedder == "basset-supervised":
         OmegaConf.set_struct(cfg, True)
         with open_dict(cfg):
             cfg.model.update(cfg.supervised_encoder[cfg.embedder])
@@ -52,40 +60,69 @@ def run_experiment(cfg: DictConfig) -> None:
     # put model on dataparallel
     if torch.cuda.device_count() > 1:
         from bend.models.downstream import CustomDataParallel
+
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         model = CustomDataParallel(model)
     print(model)
 
     # instantiate optimizer
-    optimizer =  hydra.utils.instantiate(cfg.optimizer, params = model.parameters())
-    
+    optimizer = hydra.utils.instantiate(cfg.optimizer, params=model.parameters())
+
     # define criterion
-    print(f'Use {cfg.params.criterion} loss function')
-    if cfg.params.criterion == 'cross_entropy':
-        criterion = CrossEntropyLoss(ignore_index = cfg.data.padding_value, 
-                                     weight=torch.tensor(cfg.params.class_weights).to(device) if cfg.params.class_weights is not None else None)
-    elif cfg.params.criterion == 'poisson_nll':
+    print(f"Use {cfg.params.criterion} loss function")
+    if cfg.params.criterion == "cross_entropy":
+        criterion = CrossEntropyLoss(
+            ignore_index=cfg.data.padding_value,
+            weight=(
+                torch.tensor(cfg.params.class_weights).to(device)
+                if cfg.params.class_weights is not None
+                else None
+            ),
+        )
+    elif cfg.params.criterion == "poisson_nll":
         criterion = PoissonLoss()
-    elif cfg.params.criterion == 'mse':
+    elif cfg.params.criterion == "mse":
         criterion = MSELoss()
-    elif cfg.params.criterion == 'bce':
-        criterion = BCEWithLogitsLoss(class_weights=torch.tensor(cfg.params.class_weights).to(device) if cfg.params.class_weights is not None else None)
+    elif cfg.params.criterion == "bce":
+        criterion = BCEWithLogitsLoss(
+            class_weights=(
+                torch.tensor(cfg.params.class_weights).to(device)
+                if cfg.params.class_weights is not None
+                else None
+            )
+        )
 
-    # init dataloaders 
-    if 'supervised' in cfg.embedder : cfg.data.data_dir = cfg.data.data_dir.replace(cfg.embedder, 'onehot')
-    train_loader, val_loader, test_loader = hydra.utils.instantiate(cfg.data) # instantiate dataloaders
+    # init dataloaders
+    if "supervised" in cfg.embedder:
+        cfg.data.data_dir = cfg.data.data_dir.replace(cfg.embedder, "onehot")
+    train_loader, val_loader, test_loader = hydra.utils.instantiate(
+        cfg.data
+    )  # instantiate dataloaders
+
     # instantiate trainer
-    trainer = BaseTrainer(model = model, optimizer = optimizer, criterion = criterion, 
-                        device = device, config = cfg, overwrite_dir = True)
-    
+    trainer = BaseTrainer(
+        model=model,
+        optimizer=optimizer,
+        criterion=criterion,
+        device=device,
+        config=cfg,
+        overwrite_dir=True,
+    )
 
-    if cfg.params.mode == 'train':
-        # train     
-        trainer.train(train_loader, val_loader, test_loader, cfg.params.epochs, cfg.params.load_checkpoint)
-    
-    # test 
+    if cfg.params.mode == "train":
+        # train
+        trainer.train(
+            train_loader,
+            val_loader,
+            test_loader,
+            cfg.params.epochs,
+            cfg.params.load_checkpoint,
+        )
+
+    # test
     trainer.test(test_loader, overwrite=False)
 
-if __name__ == '__main__':
-    print('Run experiment')
+
+if __name__ == "__main__":
+    print("Run experiment")
     run_experiment()
